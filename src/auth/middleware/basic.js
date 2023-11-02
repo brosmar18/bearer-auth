7eed7f0d7bbd92d32787fe0dbc31e2774db3f853
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-11-01 20:06:56 -0500

    basic-auth-middleware.test.js tests passing

--- src/auth/middleware/basic.js
+++ src/auth/middleware/basic.js
@@ -17,7 +17,7 @@ module.exports = async (req, res, next) => {
     req.user = await userModel.authenticateBasic(username, pass)
     next();
   } catch (e) {
-    console.error(e);
+    // console.error(e);
     res.status(403).send('Invalid Login');
   }
 
